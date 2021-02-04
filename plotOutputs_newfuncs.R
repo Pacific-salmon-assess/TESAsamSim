430a940e3706e3abd4ccdf5e47b0d18b26ddb192
robynforrest <robynforrest@gmail.com>
2021-02-04 14:40:23 -0800

    Adding file for new plotting functions

---
+++ plotOutputs_newfuncs.R
@@ -0,0 +1,10 @@
+# Code to make some new plots for TESAsamSim
+# Authors: Robyn Forrest and Luke Warkentin
+
+# Feb 4, 2021
+
+# load libraries - these will move to Rmd file
+library(tidyverse)
+library(purrr)
+library(reshape2)
+
