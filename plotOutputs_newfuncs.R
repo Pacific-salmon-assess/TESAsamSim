d2a08e7b2b7374c1fecdbb6ddf6fd9ccee930161
lukewarkentin <luke.warkentin@gmail.com>
2021-02-04 15:50:09 -0800

    Working on bias boxplots

--- plotOutputs_newfuncs.R
+++ plotOutputs_newfuncs.R
@@ -18,16 +18,34 @@ library(reshape2)
 
 
 
+# plot_bias_boxplots
+
 # Read in outputs of fitDLM and save boxplots of bias of estimates of alpha and beta
 # will need to change "example" to folder of automated outputs and scen to names of 
 #     scenarios from table of scenarios to loop over/map over
 
-scen <- "increaseProd_ref_CUsrDat"
+scenario <- "simDataIncreaseProdyCU1"
 
-readRDS( here("outputs","simData", "example", paste0(scen, ".RData")))$results
+plot_bias_boxplots <- function(scenario) {
 
+  dlm_out <- readRDS(here( paste0(scenario, ".Rdata")))$results # read in dlm output file (format and location subject to change)
+  
+  pms <- dlm_out %>%
+    group_by(CU, iteration) %>% # will need to add scenario column as well
+    dplyr::summarize(
+      alpha_mpb=mean((alpha_true-alpha)/alpha_true)*100,
+      beta_mpb=mean((beta_true-beta)/beta_true)*100) %>%
+    pivot_longer(alpha_mpb:beta_mpb,names_to="parameter",values_to="mpb")
 
-
+ggplot(data=pms,aes(x=factor(scenario), y=mpb, fill=parameter))+
+  geom_boxplot(outlier.shape = NA)+
+  xlab("Stock depletion") +
+  ylab("Mean % bias") +
+  coord_cartesian(ylim=c(-100,400))+
+  scale_fill_viridis_d(labels=c("alpha", "beta"))+
+  facet_wrap(~CU) +
+  theme_bw() 
+}
 
 
 
