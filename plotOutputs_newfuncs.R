4f3ea8137185a4cda4cf344a63a5534d66362a64
robynforrest <robynforrest@gmail.com>
2021-02-04 18:17:09 -0800

    Running all scenarios and putting results in dataframe

--- plotOutputs_newfuncs.R
+++ plotOutputs_newfuncs.R
@@ -4,8 +4,8 @@
 # Feb 4, 2021
 
 # Functions:
-#  
-#   plot_bias_boxplots - this reads in the dlm model output and saves boxplots showing 
+#
+#   plot_bias_boxplots - this reads in the dlm model output and saves boxplots showing
 #                         bias of estimates of alpha and beta for different scenarios
 #
 #
@@ -15,6 +15,7 @@
 library(tidyverse)
 library(purrr)
 library(reshape2)
+library(here)
 
 
 # -------------------
@@ -22,17 +23,17 @@ library(reshape2)
 # -------------------
 
 # Read in outputs of fitDLM and save boxplots of bias of estimates of alpha and beta
-# will need to change "example" to folder of automated outputs and scen to names of 
+# will need to change "example" to folder of automated outputs and scen to names of
 #     scenarios from table of scenarios to loop over/map over
 
 # scenario <- "simDataIncreaseProdyCU1"
 
 plot_bias_boxplots <- function(scenario) {
 
-  dlm_out <- readRDS(here( paste0(scenario, ".Rdata")))$results # read in dlm output file (format and location subject to change)
-  
+  dlm_out <- readRDS(here::here( paste0(scenario, ".Rdata")))$results # read in dlm output file (format and location subject to change)
+
 # x_discrete <-  # is the x variable of interest discrete?
-  
+
   pbias <- dlm_out %>%
     group_by(CU, iteration) %>% # will need to add scenario column as well
     dplyr::summarize(
@@ -40,8 +41,8 @@ plot_bias_boxplots <- function(scenario) {
       beta_mpb=mean((beta_true-beta)/beta_true)*100) %>%
     pivot_longer(alpha_mpb:beta_mpb,names_to="parameter",values_to="mpb")
 
-# if(x_discrete) == TRUE{ 
-  
+# if(x_discrete) == TRUE{
+
 fig <- ggplot(data=pbias,aes(x=factor(scenario), y=mpb, fill=parameter))+ # change scenario to variable of interest
   geom_boxplot(outlier.shape = NA)+
   # xlab("Stock depletion") +
@@ -49,7 +50,9 @@ fig <- ggplot(data=pbias,aes(x=factor(scenario), y=mpb, fill=parameter))+ # chan
   coord_cartesian(ylim=c(-100,400))+
   scale_fill_viridis_d(labels=c("alpha", "beta"))+
   facet_wrap(~paste0("CU ", CU)) +  # facet to CU number, could change to other dimension
-  theme_bw() 
+  theme_bw()
+
+fig
 #}
 
 # if(x_discrete == FALSE) {
@@ -61,7 +64,7 @@ fig <- ggplot(data=pbias,aes(x=factor(scenario), y=mpb, fill=parameter))+ # chan
 #   coord_cartesian(ylim=c(-100,400))+
 #   scale_fill_viridis_d(labels=c("alpha", "beta"))+
 #   facet_wrap(~paste0("CU ", CU)) +
-#   theme_bw() 
+#   theme_bw()
 
 #}
 
